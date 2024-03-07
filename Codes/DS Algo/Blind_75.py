11602e98e7e2ac10fb07ee231c9c1c6b1a1b8be1
forstuffapps <forstuffapps@gmail.com>
2024-03-07 12:33:41 -0500

    added problem statement to the algo

--- Codes/DS Algo/Blind_75.py
+++ Codes/DS Algo/Blind_75.py
@@ -323,6 +323,9 @@ def coinChange(coins, amount):
 
 """
 3. Longest Increasing Subsequence  (LC number - 300)
+Input: nums = [10,9,2,5,3,7,101,18]
+Output: 4
+Explanation: The longest increasing subsequence is [2,3,7,101], therefore the length is 4.
 """
 
 def lengthOfLIS(nums):
