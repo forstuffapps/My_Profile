fa29915884afbfd0a864db93cb11cf3c98be1cfa
forstuffapps <forstuffapps@gmail.com>
2024-11-18 22:21:22 -0500

    P5 E3 Tabulation Bottom Up

--- Codes/DS Algo/20 Patterns of DP.py
+++ Codes/DS Algo/20 Patterns of DP.py
@@ -1069,3 +1069,77 @@ def minDistance(word1, word2):
             
     
     return m+n-2*dp[-1][-1]     # Please check this last line which calculates the main logic of the answer
+
+
+
+"""
+LeetCode 1092: Shortest Common Supersequence
+
+Given two strings str1 and str2, return the shortest string that has both str1 and str2 as subsequences. 
+If there are multiple valid strings, return any of them.
+A string s is a subsequence of string t if deleting some number of characters from t (possibly 0) results in the string s.
+
+ 
+Example 1:
+Input: str1 = "abac", str2 = "cab"
+Output: "cabac"
+Explanation: 
+str1 = "abac" is a subsequence of "cabac" because we can delete the first "c".
+str2 = "cab" is a subsequence of "cabac" because we can delete the last "ac".
+The answer provided is the shortest such string that satisfies these properties.
+
+Example 2:
+Input: str1 = "aaaaaaaa", str2 = "aaaaaaaa"
+Output: "aaaaaaaa"
+"""
+
+
+# Tabulation Bottom Up
+
+def shortestCommonSupersequence(str1, str2):
+    a,b = str1, str2
+    m,n = len(a), len(b)
+    dp = [[0 for _ in range(n+1)] for _ in range(m+1)]
+
+    def String_length(a,b,m,n):
+
+        for i in range(m+1):
+            for j in range(n+1):
+
+                if i==0:
+                    dp[i][j] = j
+                elif j==0:
+                    dp[i][j] = i
+                elif a[i-1]==b[j-1]:
+                    dp[i][j] = 1+dp[i-1][j-1]
+                else:
+                    dp[i][j] = 1+min(dp[i-1][j], dp[i][j-1])
+        
+        return dp[m][n]
+    
+    z_len = String_length(a,b,m,n)
+    z=''
+    
+    i,j = m,n
+    while i>0 and j>0:
+        if a[i-1]==b[j-1]:
+            z = a[i-1]+z
+            i-=1
+            j-=1
+        elif dp[i-1][j]<dp[i][j-1]:
+            z = a[i-1] + z
+            i-=1
+        else:
+            z = b[j-1] + z
+            j-=1
+    
+    while i>0:
+        z = a[i-1]+z
+        i-=1
+    
+    while j>0:
+        z = b[j-1] + z
+        j-=1
+    
+
+    return z
