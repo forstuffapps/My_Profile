fd8469a15d75356dd14d6f9d1b12d47dbeddceaa
forstuffapps <forstuffapps@gmail.com>
2024-11-26 20:58:24 -0500

    P6 E2 Tabulatio Bottom Up

--- Codes/DS Algo/20 Patterns of DP.py
+++ Codes/DS Algo/20 Patterns of DP.py
@@ -1204,3 +1204,48 @@ def lengthOfLIS(nums):
 
 
 
+
+
+
+
+"""
+LeetCode 673: Number of Longest Increasing Subsequence
+
+Given an integer array nums, return the number of longest increasing subsequences.
+Notice that the sequence has to be strictly increasing.
+
+
+Example 1:
+Input: nums = [1,3,5,4,7]
+Output: 2
+Explanation: The two longest increasing subsequences are [1, 3, 4, 7] and [1, 3, 5, 7].
+
+Example 2:
+Input: nums = [2,2,2,2,2]
+Output: 5
+Explanation: The length of the longest increasing subsequence is 1, and there are 5 increasing subsequences of length 1, so output 5.
+"""
+
+# Tabulation Bottom Up
+
+def findNumberOfLIS(nums):
+    l = nums
+    n=len(l)
+    dp = [1]*n
+    cnt = [1]*n
+
+    for i in range(1,n):
+        for j in range(i):
+            if l[j]<l[i] and dp[i]<(dp[j]+1):
+                dp[i] = dp[j]+1
+                cnt[i] = cnt[j]
+            elif l[j]<l[i] and dp[i]==(dp[j]+1):
+                cnt[i]+=cnt[j]
+    
+    lis = max(dp)
+    z = 0
+    for i in range(n):
+        if dp[i]==lis:
+            z+=cnt[i]
+    
+    return z
