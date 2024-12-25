7de5ce4f03fe3ec837a1fd04fc137cc567b50c73
forstuffapps <forstuffapps@gmail.com>
2024-12-25 17:37:32 -0500

    P7 E2, Math Logic Sol

--- Codes/DS Algo/20 Patterns of DP.py
+++ Codes/DS Algo/20 Patterns of DP.py
@@ -1428,3 +1428,62 @@ def longestPalindromeSubseq(s):
     
     return dp[0][n-1]
         
+
+
+
+
+
+
+
+"""
+LeetCode 647: Palindromic Substrings
+Given a string s, return the number of palindromic substrings in it.
+A string is a palindrome when it reads the same backward as forward.
+A substring is a contiguous sequence of characters within the string.
+
+
+Example 1:
+Input: s = "abc"
+Output: 3
+Explanation: Three palindromic strings: "a", "b", "c".
+
+Example 2:
+Input: s = "aaa"
+Output: 6
+Explanation: Six palindromic strings: "a", "a", "a", "aa", "aa", "aaa".
+"""
+
+# Math Logic Solution
+
+class Solution:
+    def countSubstrings(self, s: str) -> int:
+        n = len(s)
+        cnt = 0
+        bag = set()
+
+        # Palin Function to check if a string is palindrome or not
+        def Palin(i,j):
+            cnt = 0
+            while i>=0 and j<n and s[i]==s[j]:
+                i-=1
+                j+=1
+                cnt+=1
+            
+            return cnt
+
+
+        
+        for i in range(n):
+            even_cnt = Palin(i,i+1)
+            odd_cnt = Palin(i,i)
+            cnt += even_cnt + odd_cnt
+                
+        
+        return cnt
+        
+                
+        
+        
+        
+        
+        
